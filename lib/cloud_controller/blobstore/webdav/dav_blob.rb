d08a091cda30587313aacb4cae4175003b278e6b
Simon Leung <leungs@us.ibm.com>
2016-02-10 18:00:27 -0800

    WIP

--- lib/cloud_controller/blobstore/webdav/dav_blob.rb
+++ lib/cloud_controller/blobstore/webdav/dav_blob.rb
@@ -12,8 +12,18 @@ module CloudController
       end
 
       def internal_download_url
+
+        uri = URI("http://abc:123@blobstore.service.cf.internal/sign?expire=1455159464\&prefix=/blobstore_url_signer\&path=/01/fb/01fb2bf9-00b6-4ed1-ba07-16abacbddecb")
+        client = HTTPClient.new()
+        auth = Base64.encode64( 'user:pass' ).chomp
+        client.set_auth(nil, "user", "pass")
+        response = client.get(uri, :follow_redirect => true, :Authorization => auth)
+        Steno.logger("orange-internal").info(response.body)
+        Steno.logger("orange-internal-content").info(response.content)
+
         expires   = Time.now.utc.to_i + 3600
         @signer.sign_internal_url(path: @key, expires: expires)
+
       end
 
       def public_download_url
